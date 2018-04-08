05838e3ab10d093d960a20cadefe51361f7dfbf4
Leveton <mleveton@gmail.com>
2018-04-07 22:55:18 -0700

    Update settings test

--- MapSwiftTests/SettingsViewTests.swift
+++ MapSwiftTests/SettingsViewTests.swift
@@ -7,17 +7,26 @@
 //
 
 import XCTest
+@testable import MapSwift
 
 class SettingsTests: XCTestCase {
+    var settingsVC:MSSettingsViewController?
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let coder = NSCoder()
+        settingsVC = MSSettingsViewController(coder:coder)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        settingsVC = nil
         super.tearDown()
     }
     
+    func testRange(){
+        let index = IndexPath(row: 3, section: 0)
+        let range = settingsVC?.getRangeFromIndexPath(index: index)
+        XCTAssertEqual(range?.startPoint, 3_000.0)
+        XCTAssertEqual(range?.endPoint, 10_000.0)
+    }
 }
